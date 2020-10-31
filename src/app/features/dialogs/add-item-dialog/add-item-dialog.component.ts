59c31be9af77406c42a8ab921e7c53c27b9dce65
alesc <ale.schio90@gmail.com>
2020-10-31 11:42:51 +0100

    Eslint problem fixes

--- src/app/features/dialogs/add-item-dialog/add-item-dialog.component.ts
+++ src/app/features/dialogs/add-item-dialog/add-item-dialog.component.ts
@@ -5,34 +5,34 @@ import { GraphqlService } from '../../graphql/graphql.service';
 import { GetAspectsGQL } from '../../graphql/operations';
 
 const followerAspectRequiredValidator: ValidatorFn = (fg: FormGroup) => {
-  const itemType = fg.get('itemType').value;
-  if (itemType === 'Follower') {
-    return Validators.required(fg.get('followerAspect'));
-  } else {
-    return null;
-  }
+    const itemType = fg.get('itemType').value;
+    if (itemType === 'Follower') {
+        return Validators.required(fg.get('followerAspect'));
+    } else {
+        return null;
+    }
 };
 
 @Component({
-  selector: 'app-add-item-dialog',
-  templateUrl: './add-item-dialog.component.html',
-  styleUrls: ['./add-item-dialog.component.scss']
+    selector: 'app-add-item-dialog',
+    templateUrl: './add-item-dialog.component.html',
+    styleUrls: ['./add-item-dialog.component.scss']
 })
 export class AddItemDialogComponent implements OnInit {
   itemTypes: string[] = [ 'Aspect', 'Book', 'ChangeLesson', 'Desire', 'ExpeditionObstacle', 'Follower', 'Influence', 'Ingredient',
-                          'Language', 'Location', 'Lore', 'MansusDoor', 'Rite', 'Tool' ];
+      'Language', 'Location', 'Lore', 'MansusDoor', 'Rite', 'Tool' ];
   itemTypesWithAspects: string[] = [ 'Influence', 'Ingredient', 'Lore', 'Tool' ];
   multipleAspects = {
-    Influence: true,
-    Ingredient: true,
-    Lore: false,
-    Tool: true
+      Influence: true,
+      Ingredient: true,
+      Lore: false,
+      Tool: true
   };
   itemAspectQuantities = {
-    Influence: [ 1, 2, 6, 10, 15 ],
-    Ingredient: [ 1, 2, 3, 4, 8, 12 ],
-    Lore: [ 2, 4, 6, 8, 10, 12, 14 ],
-    Tool: [ 2, 4, 8, 12 ]
+      Influence: [ 1, 2, 6, 10, 15 ],
+      Ingredient: [ 1, 2, 3, 4, 8, 12 ],
+      Lore: [ 2, 4, 6, 8, 10, 12, 14 ],
+      Tool: [ 2, 4, 8, 12 ]
   };
 
   form: FormGroup;
@@ -50,79 +50,79 @@ export class AddItemDialogComponent implements OnInit {
     private getAspectsGQL: GetAspectsGQL,
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<AddItemDialogComponent>) {
-    this.form = fb.group({
-      name: new FormControl('', [Validators.required]),
-      itemType: this.itemTypeFormControl,
-      aspects: fb.array([]),
-      language: new FormControl(''),
-      vault: new FormControl(false),
-      obstacleAspects: fb.array([ this.createObstacleAspect(), this.createObstacleAspect(), this.createObstacleAspect() ]),
-      followerAspect: new FormControl('')
-    }, {validators: [ followerAspectRequiredValidator ]});
+      this.form = fb.group({
+          name: new FormControl('', [Validators.required]),
+          itemType: this.itemTypeFormControl,
+          aspects: fb.array([]),
+          language: new FormControl(''),
+          vault: new FormControl(false),
+          obstacleAspects: fb.array([ this.createObstacleAspect(), this.createObstacleAspect(), this.createObstacleAspect() ]),
+          followerAspect: new FormControl('')
+      }, {validators: [ followerAspectRequiredValidator ]});
   }
 
   createObstacleAspect(): FormGroup {
-    return this.fb.group({
-      obstacleAspect: new FormControl('')
-    });
+      return this.fb.group({
+          obstacleAspect: new FormControl('')
+      });
   }
 
-  ngOnInit() {
-    this.itemTypeFormControl.valueChanges.subscribe(typeVal => {
-      this.loreSelected = (typeVal === 'Lore');
-      this.influenceSelected = (typeVal === 'Influence');
-      this.bookSelected = (typeVal === 'Book');
+  ngOnInit(): void {
+      this.itemTypeFormControl.valueChanges.subscribe(typeVal => {
+          this.loreSelected = (typeVal === 'Lore');
+          this.influenceSelected = (typeVal === 'Influence');
+          this.bookSelected = (typeVal === 'Book');
 
-      const aspectsFormArray = this.form.get('aspects') as FormArray;
-      aspectsFormArray.clear();
-      if (this.hasAspects(typeVal)) {
-        this.populateAspects();
-        this.addAspect();
-      }
+          const aspectsFormArray = this.form.get('aspects') as FormArray;
+          aspectsFormArray.clear();
+          if (this.hasAspects(typeVal)) {
+              this.populateAspects();
+              this.addAspect();
+          }
 
-      if (this.bookSelected) {
-        this.service.getLanguages().then(list => this.languages = list);
-      }
-      if (typeVal === 'ExpeditionObstacle' || typeVal === 'Follower') {
-        this.populateAspects();
-      }
-    });
+          if (this.bookSelected) {
+              this.service.getLanguages().then(list => this.languages = list);
+          }
+          if (typeVal === 'ExpeditionObstacle' || typeVal === 'Follower') {
+              this.populateAspects();
+          }
+      });
   }
 
   private async populateAspects() {
-    const queryResult = await this.getAspectsGQL.fetch().toPromise();
-    this.aspects = queryResult.data.Aspect.map(aspect => aspect.name);
+      const queryResult = await this.getAspectsGQL.fetch().toPromise();
+      this.aspects = queryResult.data.Aspect.map(aspect => aspect.name);
   }
 
   hasAspects(itemType: string): boolean {
-    return this.itemTypesWithAspects.includes(itemType);
+      return this.itemTypesWithAspects.includes(itemType);
   }
 
   private createAspect(): FormGroup {
-    return this.fb.group({
-      aspect: new FormControl('', Validators.required),
-      quantity: new FormControl('', Validators.required)
-    });
+      return this.fb.group({
+          aspect: new FormControl('', Validators.required),
+          quantity: new FormControl('', Validators.required)
+      });
   }
 
-  public addAspect() {
-    const rewards = this.form.get('aspects') as FormArray;
-    rewards.push(this.createAspect());
+  public addAspect(): void {
+      const rewards = this.form.get('aspects') as FormArray;
+      rewards.push(this.createAspect());
   }
 
-  save() {
-    this.form.updateValueAndValidity();
-    if (this.form.valid) {
-      this.dialogRef.close(this.form.value);
-    }
+  save(): void {
+      this.form.updateValueAndValidity();
+      if (this.form.valid) {
+          this.dialogRef.close(this.form.value);
+      }
   }
 
-  public removeAspect(index: number) {
-    const rewards = this.form.get('aspects') as FormArray;
-    rewards.removeAt(index);
+  public removeAspect(index: number): void {
+      const rewards = this.form.get('aspects') as FormArray;
+      rewards.removeAt(index);
   }
 
-  close() {
-    this.dialogRef.close();
+  close(): void{
+      this.dialogRef.close();
   }
 }
